8693224028ee2b60184b74a244b427d321816f25
junwooji <junwoo@finda.co.kr>
2018-08-12 00:08:01 +0900

    implements Header Component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import logo from './logo.svg';
+import Header from './components/Header';
+import logo from './assets/inko_logo.svg';
 
 const Title = styled.div`
   text-align: center;
 `;
 
 const LogoTitle = styled.img`
-  height: 256px;
+  margin-top: 110px;
+  margin-bottom: 20px;
+  height: 220px;
 `;
 
 const Subtitle = styled.div`
   font-size: 24px;
   font-weight: bold;
-  margin-top: 20px;
 `;
 
 const Description = styled.div`
   font-size: 20px;
 `;
 
+const subtitle = "English into Korea Converter Opensource library";
+const description = "It converts mistakenly misspelled english letters into korean or conversely";
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Title>
           <LogoTitle src={logo} />
-          <Subtitle>English into Korea Converter Opensource library</Subtitle>
-          <Description>It converts mistakenly misspelled english letters into korean or conversely</Description>
+          <Subtitle>{subtitle}</Subtitle>
+          <Description>{description}</Description>
         </Title>
       </div>
     );
--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo_bird from '../assets/inko_logo_bird.svg';
+import logo_word from '../assets/inko_logo_word.svg';
+import github_logo from '../assets/github_logo.svg';
+
+const Container = styled.div`
+    height: 80px;
+    width: 100%
+    background: transparent;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+const LogoImage = styled.img`
+    height: 60px;
+    margin: 10px 20px;
+`;
+
+const LogoWording = styled.img`
+    height: 35px;
+`;
+
+const Github = styled.img`
+    height: 20px;
+    margin-right: 20px;
+`
 
 class Header extends React.Component {
     render() {
-      return (
-        <div>
-          
-        </div>
-      );
+        return (
+            <div>
+                <Container>
+                    <Logo>
+                        <LogoImage src={logo_bird} />
+                        <LogoWording src={logo_word} />
+                    </Logo>
+                    <a href="https://github.com/JonJee/inko" target="_blank"><Github src={github_logo} /></a>
+                </Container>
+            </div>
+        );
     }
-  }
+}
+
+export default Header;
